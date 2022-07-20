b60d43aae7d520b95304df853c86bde51c93b457
miqiang1986 <895318625@qq.com>
2022-07-20 20:10:45 +0800

    添加gateway模块

--- baoding/README.md
+++ baoding/README.md
@@ -2,5 +2,9 @@
 
 用户权限、登录模块
 
+注意：
+    运行jar包时命令中添加参数： -Dfile.encoding=UTF-8
+        java -Dfile.encoding=UTF-8 -jar baoding-0.0.0-SNAPSHOT.jar
+
 
 
---
+++ dingzhou/src/main/java/com/miqiang/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.miqiang.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author miqiang
+ * @version 1.0
+ * @description
+ * @createTime 2022-07-20  17:03
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
+
